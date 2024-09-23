73fcc930455c006d8be280042445e0ad5da24eca
Barkeotullah Opu <146110521+Barkotullah02@users.noreply.github.com>
2024-09-23 10:34:23 +0600

    New flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                color: Colors.white60,
+                child: Text("container-1"),
+                height: 100.0,
+                width: 100.0,
+              ),
+              Container(
+                color: Colors.blue,
+                child: Text("container-2"),
+                height: 100.0,
+                width: 100.0,
+              ),
+              Container(
+                color: Colors.green,
+                child: Text("container-2"),
+                height: 100.0,
+                width: 100.0,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+
